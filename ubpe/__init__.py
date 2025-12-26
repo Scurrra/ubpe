1249c2eebdba332563bff27f51a4470f0e4a88ad
Scurrra <iscurrra@gmail.com>
2025-12-26 21:12:28 +0300

    v0.2.0-rc1

--- ubpe/__init__.py
+++ ubpe/__init__.py
@@ -1,10 +1,29 @@
-__version__ = "0.1.1"
+__version__ = "0.2.0-rc1"
+
 
 try:
-    from ubpe_native import UBPE, UBPEClassic # type: ignore
+    from ubpe_cyton import UBPE, UBPEClassic  # type: ignore
 except ImportError:
+    _has_ubpe_cython = False
+else:
+    _has_ubpe_cython = True
+
+
+try:
+    if _has_ubpe_cython:
+        from ubpe_native import UBPE, UBPEClassic  # type: ignore
+    else:
+        raise ImportError()
+except ImportError:
+    _has_ubpe_native = False
+else:
+    _has_ubpe_native = True
+
+
+if not _has_ubpe_cython and not _has_ubpe_native:
     raise Exception(
-        "Implementation package was not found. Make sure that you are installing the package with optional dependency: `pip install ubpe[native]`"
+        "Implementation package was not found. Make sure that you are installing the package with optional dependency: `pip install ubpe[native]` or `pip install ubpe[cython]`"
     )
+
 
 __all__ = ["UBPEClassic", "UBPE"]
